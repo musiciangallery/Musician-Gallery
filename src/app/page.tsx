849a107e28a6c7f59ec9b049fb108b62b5c946a5
musiciangallery <contact@musiciangallery.co.nz>
2026-07-16 15:50:02 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,10 +32,7 @@ export default async function Home() {
             <br />
             <em>musicians.</em>
           </h1>
-          <p className="max-w-lg text-sm text-mid mb-8">
-            A considered introduction between talented musicians and the
-            people looking for them.
-          </p>
+          <p className="max-w-lg text-sm text-mid mb-8">A considered introduction between talented musicians and the people looking for them. Softly launching, September 2026.</p>
           <div className="flex flex-wrap gap-6">
             <Link
               href="/gallery"
